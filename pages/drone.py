4836104c45c009ba50fd77fd3b768e50a130abb2
Petr Rybakov <prybakov@miem.hse.ru>
2023-12-03 23:15:36 +0300

    Added inference over a directory of images for drone demo page

--- pages/drone.py
+++ pages/drone.py
@@ -4,30 +4,53 @@ import streamlit as st
 import settings
 import drone_helper
 import numpy as np
-
+import utils
 big_model, small_model = drone_helper.init_models(settings.DRONE_BIG_MODEL, settings.DRONE_SMALL_MODEL)
+
+images_directory_path = Path("media/images/drone")
 
 def drone_demo():
     st.set_page_config(layout="wide")
 
-    image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
-    if not image_file:
-        return None
+    source_type = st.sidebar.radio(
+    "Тип источника", ['Файл', 'Папка'])
 
-    original_image = Image.open(image_file)
+    if source_type == 'Файл':
+        image_file = st.file_uploader("Upload Your Image", type=['jpg', 'png', 'jpeg'])
+        if not image_file:
+            return None
 
-    
-     
-    prepared_image = drone_helper.prepare_image(original_image)
-    results, cutout_images, big_bboxes = drone_helper.inference(big_model, small_model, prepared_image)
-    cutout_images = drone_helper.preprocess_cutout_images(cutout_images)
+        original_image = Image.open(image_file)
 
-    processed_image = drone_helper.draw_bboxes(np.array(original_image), results, big_bboxes)
+        prepared_image = drone_helper.prepare_image(original_image)
+        results, cutout_images, big_bboxes = drone_helper.inference(big_model, small_model, prepared_image)
+        cutout_images = drone_helper.preprocess_cutout_images(cutout_images)
 
-    col1, col2 = st.columns(2)
-    st.text("Detected people")
-    col1.image(original_image, caption = "Original image")
-    col2.image(processed_image, caption = "Detection results")
-    st.image(cutout_images, clamp=True)
-    
+        processed_image = drone_helper.draw_bboxes(np.array(original_image), results, big_bboxes)
+
+        col1, col2 = st.columns(2)
+        st.text("Обнаруженные люди")
+        col1.image(original_image, caption = "Исходное изображение")
+        col2.image(processed_image, caption = "Результаты распознавания")
+        st.image(cutout_images, clamp=True)
+    elif source_type == 'Папка':
+        st_frame = st.empty()
+        for original_image in utils.iterator_from_images_directory(images_directory_path):
+                if not original_image:
+                    return None
+
+                prepared_image = drone_helper.prepare_image(original_image)
+                results, cutout_images, big_bboxes = drone_helper.inference(big_model, small_model, prepared_image)
+                cutout_images = drone_helper.preprocess_cutout_images(cutout_images)
+
+                processed_image = drone_helper.draw_bboxes(np.array(original_image), results, big_bboxes)
+                with st_frame:
+                    st.text("Обнаруженные люди")
+                    col1, col2 = st.columns(2)
+                    col1.image(original_image, caption = "Исходное изображение")
+                    col2.image(processed_image, caption = "Результаты распознавания")
+                    # st.image(cutout_images, clamp=True)
+    else:
+        st.error("Please select a valid source type!")
+
 drone_demo()
