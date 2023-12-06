e39fd31ce974f2bc15b527acbe58f3e890922475
Petr Rybakov <prybakov@miem.hse.ru>
2023-12-06 04:37:44 +0300

    Added mean inference time calculation to drone page demo

--- pages/drone.py
+++ pages/drone.py
@@ -5,9 +5,15 @@ import settings
 import drone_helper
 import numpy as np
 from helper import iterator_from_images_directory
+from time import perf_counter
+from statistics import mean
+
 big_model, small_model = drone_helper.init_models(settings.DRONE_BIG_MODEL, settings.DRONE_SMALL_MODEL)
 
 images_directory_path = Path("media/images/drone")
+images_directory_path = Path("media/images/drone_closed_val")
+
+inference_time_list = []
 
 def drone_demo():
     st.set_page_config(layout="wide")
@@ -38,11 +44,14 @@ def drone_demo():
         for original_image in iterator_from_images_directory(images_directory_path):
                 if not original_image:
                     return None
-
+                
                 prepared_image = drone_helper.prepare_image(original_image)
+                timestamp_before_inference = perf_counter()
                 results, cutout_images, big_bboxes = drone_helper.inference(big_model, small_model, prepared_image)
+                inference_time = perf_counter() - timestamp_before_inference
+                print(f"Inference time: {inference_time * 1000:.0f}ms")
+                inference_time_list.append(inference_time)
                 cutout_images = drone_helper.preprocess_cutout_images(cutout_images)
-
                 processed_image = drone_helper.draw_bboxes(np.array(original_image), results, big_bboxes)
                 with st_frame:
                     st.text("Обнаруженные люди")
@@ -50,6 +59,7 @@ def drone_demo():
                     col1.image(original_image, caption = "Исходное изображение")
                     col2.image(processed_image, caption = "Результаты распознавания")
                     # st.image(cutout_images, clamp=True)
+        print(f"Mean inference time: {mean(inference_time_list) * 1000:.0f}ms")
     else:
         st.error("Please select a valid source type!")
 
